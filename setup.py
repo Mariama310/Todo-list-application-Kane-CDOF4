f6531be9fa57458ac23dfdb7cffd2c04cda869bf
Mariama310 <sokhna-mariama-bousso.kane@edu.devinci.fr>
2024-02-06 17:16:45 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Todo-list-application-Kane-CDOF4',
+    version='1.0',
+    author='Mariama310',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
